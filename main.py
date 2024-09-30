e6d0de46202dbbd725d4d52d7a3871c439da1ad6
Matthew Gorkowski <matt@gorkowski.com>
2024-09-30 13:45:30 -0700

    Added asteroids and player classes

--- main.py
+++ main.py
@@ -3,19 +3,47 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for obj in updatable:
+            obj.update(dt)
             
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
+        # limit the frameratre to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
